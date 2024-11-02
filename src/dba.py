49b305ea557d5ae7e95765073dc5d3d76269939d
Miguel Hernandez <migher74@outlook.com>
2024-11-01 20:12:02 -0600

    Create the function to create the data table, if not exists in the database.

--- src/dba.py
+++ src/dba.py
@@ -4,3 +4,18 @@ import sqlite3
 def connect():
     connection = sqlite3.connect("./db/logins.db")
     return connection
+
+
+def existing_table():
+    sqldb = """CREATE TABLE IF NOT EXISTS users (
+    user_id TEXT NOT NULL,
+    user_name TEXT NOT NULL,
+    user_password TEXT NOT NULL,
+    CONSTRAINT pk_user_id PRIMARY KEY (user_id)
+    );"""
+
+    condb = connect()
+    curdb = condb.cursor()
+    curdb.execute(sqldb)
+    condb.commit()
+    condb.close()
