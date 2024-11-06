896fec0b55870863f4f750e64a3ea6202739be9b
Miguel Hernandez <migher74@outlook.com>
2024-11-05 23:18:02 -0600

    Create the users list frame contains a treeview and a scrollbar.

--- forms/users_form.py
+++ forms/users_form.py
@@ -1,4 +1,4 @@
-from ttkbootstrap import Toplevel, Frame, Label, Entry
+from ttkbootstrap import Toplevel, Frame, Label, Entry, Treeview, Scrollbar
 
 
 class MyUsers(Toplevel):
@@ -26,3 +26,26 @@ class MyUsers(Toplevel):
 
         self.nameEntry = Entry(self.entryFrame, width=40)
         self.nameEntry.grid(row=1, column=1)
+
+        # Users List Frame
+        self.tblFrame = Frame(self)
+        self.tblFrame.grid(row=1, column=0)
+
+        self.tblUser = Treeview(self.tblFrame, columns=(1, 2),
+                                show="headings", height=15,
+                                selectmode="browse")
+
+        self.tblUser.heading(1, text="Id")
+        self.tblUser.heading(2, text="Name")
+
+        self.tblUser.column(1, width=100)
+        self.tblUser.column(2, width=200)
+
+        self.tblUser.pack(side="left")
+
+        # ScrollBar
+        self.sbrUser = Scrollbar(self.tblFrame, orient="vertical")
+        self.sbrUser.pack(side="right", fill="y")
+
+        self.tblUser.config(yscrollcommand=self.sbrUser.set)
+        self.sbrUser.config(command=self.tblUser.yview)
