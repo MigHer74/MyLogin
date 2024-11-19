e38bdec7dfe4522e107329ad907088bda5c16221
Miguel Hernandez <migher74@outlook.com>
2024-11-18 22:25:52 -0600

    Add ttkbootstrap build-in methods for the initial behavior of the window.

--- forms/about_form.py
+++ forms/about_form.py
@@ -4,3 +4,7 @@ from ttkbootstrap import Toplevel, Frame, Label, Button
 class MyAbout(Toplevel):
     def __init__(self, parent):
         super().__init__(title="About", resizable=(False, False))
+        self.place_window_center()
+        self.transient(parent)
+        self.grab_set()
+        self.focus()
